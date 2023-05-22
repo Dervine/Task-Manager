63cd70f0c6ab819bdaf0b72426adb91a8c344753
Dervine <dervine@Dervines-MacBook-Pro.local>
2023-05-22 16:18:09 +0300

    vue js frontend set up

---
+++ frontend/src/components/CreateUser.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-card elevation="2" >
+        <form @submit.prevent="createUser">
+            <label for="name">Name:</label>
+            <input type="text" id="name" v-model="newUser.name" required>
+            <button type="submit">Create</button>
+        </form>
+    </v-card>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        newUser: {
+          name: '',
+          email: '',
+          password: ''
+        }
+      };
+    },
+    methods: {
+        createUser() {
+        // Perform the create operation using API or other logic
+        // Example: this.$http.post('/api/items', this.newUser)
+        // Reset the form after successful creation
+        this.newUser.name = '';
+      }
+    }
+  };
+  </script>
